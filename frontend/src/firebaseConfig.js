49b72e12229345eae4e1aaf239578ac74bd224e3
P.C.K. Thilakarathna <thilakarathnackh@gmail.com>
2026-05-19 01:15:16 +0530

    Security_updated_3

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -3,16 +3,17 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+console.log("Vite sees my API Key as:", import.meta.env.VITE_FIREBASE_apiKey);
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAoERlJrGzNjEgj_wwtO9TtLdYh6ZZSUIE",
-  authDomain: "flutter-gp-dc346.firebaseapp.com",
-  projectId: "flutter-gp-dc346",
-  storageBucket: "flutter-gp-dc346.firebasestorage.app",
-  messagingSenderId: "331425841246",
-  appId: "1:331425841246:web:fc0b52b15bc1d7c32fb4cf",
-  measurementId: "G-85C39J7SS8"
+  apiKey: import.meta.env.VITE_FIREBASE_apiKey,
+  authDomain: import.meta.env.VITE_FIREBASE_authDomain,
+  projectId: import.meta.env.VITE_FIREBASE_projectId,
+  storageBucket: import.meta.env.VITE_FIREBASE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_messagingSenderId,
+  appId: import.meta.env.VITE_FIREBASE_appId,
+  measurementId: import.meta.env.VITE_FIREBASE_measurementId
 };
 
 // Initialize Firebase
